eb2e5066ff1f96729c5a346d82d2acba3374d1c6
jolu2111 <jolu2111@gmail.com>
2024-11-14 17:41:29 +0100

    sammenligne PLAXIS med NN

---
+++ model_definition.py
@@ -0,0 +1,15 @@
+import torch
+import torch.nn as nn
+
+class pinn(nn.Module):
+    def __init__(self, hidden_size=128, hidden_layers=3):
+        super(pinn, self).__init__()
+        layers = [nn.Linear(2, hidden_size), nn.Tanh()]
+        for _ in range(hidden_layers - 1):
+            layers.append(nn.Linear(hidden_size, hidden_size))
+            layers.append(nn.Tanh())
+        layers.append(nn.Linear(hidden_size, 1))
+        self.network = nn.Sequential(*layers)
+        
+    def forward(self, x):
+        return self.network(x)
