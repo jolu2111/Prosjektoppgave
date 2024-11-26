f9e9142c0f000176f699b11a5a650bc7fa28d78d
jolu2111 <jolu2111@gmail.com>
2024-11-26 21:54:37 +0100

    Made all the results

--- model_definition.py
+++ model_definition.py
@@ -2,12 +2,12 @@ import torch
 import torch.nn as nn
 
 class pinn(nn.Module):
-    def __init__(self, hidden_size=128, hidden_layers=3):
+    def __init__(self, hidden_size=20, hidden_layers=3):
         super(pinn, self).__init__()
         layers = [nn.Linear(2, hidden_size), nn.Tanh()]
-        for _ in range(hidden_layers - 1):
+        for _ in range(hidden_layers):
             layers.append(nn.Linear(hidden_size, hidden_size))
-            layers.append(nn.Tanh())
+            layers.append(nn.Tanh())  
         layers.append(nn.Linear(hidden_size, 1))
         self.network = nn.Sequential(*layers)
         
